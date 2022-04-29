9d0f792731e54a6a026f94d5870e58f07dd78477
elspgoco0915 <elspgoco0915@gmail.com>
2022-04-30 07:01:43 +0900

    loading修正

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,18 @@
 var showloading = document.getElementById('loading');
 var contents = document.getElementById('index');
 
+// スライドの画像を先に読み込んでおく
+var images = [
+  "images/main_backimg1.jpg",
+  "images/main_backimg2.jpg",
+  "images/main_backimg3.jpg",
+  "images/main_backimg4.jpg",
+];
+for($i = 0; $i < images.length; $i++){
+  console.log($i);
+  document.getElementById("index").style.backgroundImage = "url(" + setImage[$i] + ")";
+}
+
 window.addEventListener('load', function () {
   showloading.style.display = 'none';
   contents.classList.remove('hidden');
